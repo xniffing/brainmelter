301c002ce71a9fe67c7870638348fb40dd12f0cc
David Vicente <david.vicente@siemens.com>
2019-12-12 17:26:31 +0000

    Composition API

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCompositionApi from '@vue/composition-api';
 import './assets/tailwind.css'
 
+
+Vue.use(VueCompositionApi);
 Vue.config.productionTip = false
 
 new Vue({
